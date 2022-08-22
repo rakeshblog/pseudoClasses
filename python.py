4e968b1edddccd26143c14245ce5b5492485b7d5
rakesh kuamr <keshavkumar.1611@gmail.com>
2022-08-22 14:23:04 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,11 @@
+	
+	def inspect(x)
+	if x==0:
+	print(x, "this is zero number")
+	elif x>0:
+	print (x, "this is Positive numvber")
+	elif x<0:
+	print (x, "this is negative number")
+	else:
+	print (x,"Not define number")
+	
